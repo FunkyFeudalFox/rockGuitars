26c2d077e5c94b8a27a1caa62cae6a7a81886c7e
FunkyFeudalFox <arjan_tammer@yahoo.co.uk>
2018-07-04 11:01:37 +0200

    GuitarController updated to work with CredRepository version of GuitarRepository

--- src/main/java/com/capgemini/rockGuitars/persistence/GuitarRepository.java
+++ src/main/java/com/capgemini/rockGuitars/persistence/GuitarRepository.java
@@ -1,69 +1,9 @@
 package com.capgemini.rockGuitars.persistence;
 
 import com.capgemini.rockGuitars.model.Guitar;
-import org.hibernate.dialect.FirebirdDialect;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PostConstruct;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
 @Repository
-public class GuitarRepository {
-
-
-    //In real life there would be a database here, like Oracle, MySQL or Postgres
-
-    private long lastId = 0;
-
-    private Map<Long, Guitar> guitars = new HashMap<>();
-
-    @PostConstruct
-    public void init(){
-        for(int i = 0; i < 5; i++){
-            Guitar guitar = new Guitar();
-            guitar.setName("Twonky " + (i + 1));
-            guitar.setNumberOfStrings(6);
-            guitar.setId(i + 1);
-
-            this.save(guitar);
-        }
-    }
-
-
-    public void save(Guitar newGuitar){
-
-        newGuitar.setId(++lastId);
-        this.guitars.put(newGuitar.getId(), newGuitar);
-    }
-
-
-
-    public Guitar findById(long id){
-
-        return this.guitars.get(id);
-    }
-
-    public void deleteById(long id){
-
-        this.guitars.remove(id);
-    }
-
-    public Guitar update(long id, Guitar update){
-
-        Guitar directObject = this.findById(id);  //directObject = lijdend voorwerp
-        directObject.setName(update.getName());
-        directObject.setNumberOfStrings(update.getNumberOfStrings());
-
-        return directObject;
-    }
-
-    public Collection findAll(){
-
-        return this.guitars.values();
-    }
+public interface GuitarRepository extends CrudRepository<Guitar, Long> {
 }
-
-
--- src/main/java/com/capgemini/rockGuitars/rest/GuitarController.java
+++ src/main/java/com/capgemini/rockGuitars/rest/GuitarController.java
@@ -3,6 +3,8 @@ package com.capgemini.rockGuitars.rest;
 import com.capgemini.rockGuitars.model.Guitar;
 import com.capgemini.rockGuitars.persistence.GuitarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +18,12 @@ public class GuitarController {
     @Autowired
     private GuitarRepository guitarRepository;
 
+    /*
     @GetMapping("name")
     public String getHello(){
         return "Hello world!";
     }
 
-    /*
     @GetMapping("guitar")
     public Guitar getGuitar() {
         Guitar bcRichWarlock = new Guitar();
@@ -33,34 +35,56 @@ public class GuitarController {
     */
 
     @PostMapping
-    public Guitar create(@RequestBody Guitar newGuitar){
+    public ResponseEntity<Guitar> create(@RequestBody Guitar newGuitar){
 
         newGuitar.setId(++Guitar.lastId);
         this.guitarRepository.save(newGuitar);
 
-        return newGuitar;
+        return new ResponseEntity<Guitar>(newGuitar, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public Collection<Guitar> guitarList(){
-        return this.guitarRepository.findAll();
+    public ResponseEntity<Iterable<Guitar>> guitarList(){
+        return new ResponseEntity<Iterable<Guitar>>(this.guitarRepository.findAll(), HttpStatus.OK );
     }
 
     @GetMapping("{id}")
-    public Guitar findById(@PathVariable long id){
-        Guitar result = this.guitarRepository.findById(id);
-        return result;
+    public ResponseEntity<Guitar> findById(@PathVariable long id) {
+        Optional<Guitar> result = this.guitarRepository.findById(id);
+        if (result.isPresent()){
+            return new ResponseEntity<Guitar>(result.get(), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("{id}")
-    public Guitar updateById(@PathVariable long id, @RequestBody Guitar update){
+    public ResponseEntity<Guitar> updateById(@PathVariable long id, @RequestBody Guitar update){
 
-        return this.guitarRepository.update(id, update);
+        Optional<Guitar> possibleDirectObject = this.guitarRepository.findById(id);
+
+        if (possibleDirectObject.isPresent()) {
+            Guitar directObject = possibleDirectObject.get();
+
+            directObject.setName(update.getName());
+            directObject.setNumberOfStrings(update.getNumberOfStrings());
+            directObject.setId(update.getId());
+
+            directObject = this.guitarRepository.save(directObject);
+
+            return new ResponseEntity<Guitar>(this.guitarRepository.save(directObject),
+                    HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<Guitar>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteById(@PathVariable long id){
+    public ResponseEntity<?> deleteById(@PathVariable long id){
         this.guitarRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
